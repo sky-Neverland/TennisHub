0ef49efdf3bbb82a6a5b628df54a2c0119e1241f
sky <48169106+sky-Neverland@users.noreply.github.com>
2023-12-05 13:42:17 -0600

    Add tabs (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,25 @@
 import React, { useState, useEffect, useRef } from "react";
-import ModelCard from "./ModelCard/ModelCard";
-import HeaderSearch from "./HeaderSearch/HeaderSearch";
-import Dropzone from "./Dropzone/Dropzone";
-import Grid from "./Grid/Grid";
-import { FILES } from "./constants";
-import { getVideos, uploadVideo } from "./ModelCard/utils";
-import { IFile } from "./types";
+import { Tabs } from "@mantine/core";
+import PublicTab from "./PublicTab/PublicTab";
+import PrivateTab from "./PrivateTab/PrivateTab";
 
 const App = () => {
-    const [value, setValue] = useState("");
-    const [file, setFile] = useState(FILES);
-    const renderRef = useRef(true);
-    const onDrop = (newFile: IFile) => uploadVideo(file, setFile, newFile);
-    useEffect(() => {
-        if (renderRef.current) getVideos(file, setFile);
-        return () => {
-            renderRef.current = false;
-        };
-    }, []);
-
     return (
-        <>
-            <HeaderSearch setValue={setValue} data={file} />
-            <Dropzone onDrop={onDrop} />
-            <Grid>
-                {file.map((file) => {
-                    return (
-                        <ModelCard
-                            key={file.name}
-                            title={file.name}
-                            display={file.name.includes(value)}
-                        >
-                            <iframe
-                                src={file.src}
-                                title="YouTube video player"
-                                style={{ border: 0 }}
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                allowFullScreen
-                            ></iframe>
-                        </ModelCard>
-                    );
-                })}
-            </Grid>
-        </>
+        <Tabs defaultValue="public">
+            <Tabs.List>
+                <Tabs.Tab value="public">Public videos</Tabs.Tab>
+
+                <Tabs.Tab value="private">Private videos</Tabs.Tab>
+            </Tabs.List>
+
+            <Tabs.Panel value="public">
+                <PublicTab />
+            </Tabs.Panel>
+
+            <Tabs.Panel value="private">
+                <PrivateTab />
+            </Tabs.Panel>
+        </Tabs>
     );
 };
 

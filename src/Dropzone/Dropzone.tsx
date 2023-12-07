bc4d0d32b13091dba6a104b94858d06852f002ab
sky <48169106+sky-Neverland@users.noreply.github.com>
2023-12-07 17:49:42 -0600

    Get, Upload (#4)

--- src/Dropzone/Dropzone.tsx
+++ src/Dropzone/Dropzone.tsx
@@ -1,15 +1,25 @@
 import React, { useRef, useState, FC } from "react";
+import classnames from "classnames";
 import { Text, Group, createStyles, rem } from "@mantine/core";
 import { Dropzone as Drop, MIME_TYPES, FileWithPath } from "@mantine/dropzone";
-import { IconCloudUpload, IconX, IconDownload } from "@tabler/icons-react";
+import {
+    IconCloudUpload,
+    IconX,
+    IconDownload,
+    IconUserOff,
+} from "@tabler/icons-react";
 import Preview from "./Preview/Preview";
-import { IFile } from "../types";
+import { IUploadRequest, UploadState } from "../types";
 
 const MAX_SIZE = 8; // File size in MB
 const useStyles = createStyles((theme) => ({
     dropzone: {
         borderWidth: rem(1),
         paddingBottom: rem(50),
+    },
+    disabled: {
+        opacity: 0.5,
+        cursor: "not-allowed",
     },
 
     icon: {
@@ -20,25 +30,48 @@ const useStyles = createStyles((theme) => ({
     },
 }));
 
-const Dropzone: FC<{ onDrop: (q: IFile) => void }> = ({ onDrop }) => {
+const toBase64: (file: File) => Promise<string> = (file) =>
+    new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result?.toString() || "");
+        reader.onerror = reject;
+    });
+
+const Dropzone: FC<{
+    onDrop: (q: IUploadRequest) => void;
+    loading?: boolean;
+    disabled?: boolean;
+    uploadState?: string;
+}> = ({ onDrop, loading, disabled, uploadState }) => {
     const { classes, theme } = useStyles();
     const openRef = useRef<() => void>(null);
     const [files, setFiles] = useState<FileWithPath[]>([]);
 
-    const onDropVideo = (files: FileWithPath[]) => {
-        setFiles(files);
-        const newFile = files[files.length - 1];
-        onDrop({
-            name: newFile.name,
-            src: URL.createObjectURL(newFile) || "",
-        });
+    const onDropVideo = async (files: FileWithPath[]) => {
+        try {
+            setFiles(files);
+            const newFile = files[files.length - 1];
+            const data = await toBase64(newFile);
+            onDrop({
+                assetname: newFile.name,
+                isPublic: false,
+                data: data.replace("data:video/mp4;base64,", ""),
+            });
+        } catch (e) {
+            console.log(e);
+        }
     };
-
     return (
         <Drop
+            disabled={disabled}
+            loading={loading}
             openRef={openRef}
             onDrop={onDropVideo}
-            className={classes.dropzone}
+            className={classnames(
+                classes.dropzone,
+                disabled && classes.disabled
+            )}
             radius="md"
             accept={[MIME_TYPES.mp4]}
             maxSize={MAX_SIZE * 1024 ** 2}
@@ -61,15 +94,30 @@ const Dropzone: FC<{ onDrop: (q: IFile) => void }> = ({ onDrop }) => {
                                 />
                             ))
                         ) : (
-                            <IconCloudUpload
-                                size={rem(50)}
-                                color={
-                                    theme.colorScheme === "dark"
-                                        ? theme.colors.dark[0]
-                                        : theme.black
-                                }
-                                stroke={1.5}
-                            />
+                            <>
+                                {disabled && (
+                                    <IconUserOff
+                                        size={rem(50)}
+                                        color={
+                                            theme.colorScheme === "dark"
+                                                ? theme.colors.dark[0]
+                                                : theme.black
+                                        }
+                                        stroke={1.5}
+                                    />
+                                )}
+                                {!disabled && (
+                                    <IconCloudUpload
+                                        size={rem(50)}
+                                        color={
+                                            theme.colorScheme === "dark"
+                                                ? theme.colors.dark[0]
+                                                : theme.black
+                                        }
+                                        stroke={1.5}
+                                    />
+                                )}
+                            </>
                         )}
                     </Drop.Idle>
                     <Drop.Accept>
@@ -84,11 +132,41 @@ const Dropzone: FC<{ onDrop: (q: IFile) => void }> = ({ onDrop }) => {
                 <Text ta="center" fw={700} fz="lg" mt="xl">
                     <Drop.Accept>Drop files here</Drop.Accept>
                     <Drop.Reject>File less than {MAX_SIZE}MB</Drop.Reject>
-                    <Drop.Idle>Upload video</Drop.Idle>
+                    <Drop.Idle>
+                        {disabled ? "User not selected" : "Upload video"}
+                    </Drop.Idle>
                 </Text>
                 <Text ta="center" fz="sm" mt="xs" c="dimmed">
-                    Upload a video in mp4 format
+                    {disabled
+                        ? "Select a user to upload video"
+                        : "Upload a video in mp4 format"}
                 </Text>
+                {uploadState !== UploadState.IDLE && (
+                    <>
+                        {uploadState === UploadState.FAILED && (
+                            <Text
+                                ta="center"
+                                fw={700}
+                                fz="lg"
+                                mt="xl"
+                                color="red"
+                            >
+                                Upload Failed
+                            </Text>
+                        )}
+                        {uploadState === UploadState.DONE && (
+                            <Text
+                                ta="center"
+                                fw={700}
+                                fz="lg"
+                                mt="xl"
+                                color="green"
+                            >
+                                Uploaded
+                            </Text>
+                        )}
+                    </>
+                )}
             </div>
         </Drop>
     );
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,52 @@
+export enum TrackState {
+    UNTRACKED = "untracked",
+    PENDING = "pending",
+    DONE = "done",
+}
+
 export interface IFile {
-    name: string;
-    src: string;
+    assetid: string;
+    userid: string;
+    assetname: string;
+    tracked: TrackState;
+    isPublic: boolean;
+    org_video_url: string;
+    new_video_url: string;
+}
+
+export interface IUser {
+    userid: string;
+    email: string;
+    lastname: string;
+    firstname: string;
+    bucketfolder: string;
+}
+
+export interface IUserResponse {
+    message: string;
+    data: IUser[];
+}
+
+export interface IFileResponse {
+    message: string;
+    data: IFile[];
+}
+
+export interface IUploadRequest {
+    assetname: string;
+    data: string;
+    isPublic: boolean;
+}
+
+export enum RDS_API {
+    LIST_PUBLIC_VIDEOS = "/list_public_videos",
+    LIST_USER_VIDEOS = "/list_user_videos",
+    GET_USERS = "/users",
+    UPLOAD_VIDEO = "/upload_org_video",
+}
+
+export enum UploadState {
+    IDLE = "idle",
+    DONE = "done",
+    FAILED = "failed",
 }
